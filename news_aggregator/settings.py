65793e94049205aa231fd85fcf410a7150eee1f8
johndoe <johndoe@email.com>
2026-03-30 11:52:22 +0300

    Обновление 2

--- news_aggregator/settings.py
+++ news_aggregator/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vw5i+tve@x)$s05(rfk=&wf5y1zua%+-xx$c)kfyixm=qb4#c-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
