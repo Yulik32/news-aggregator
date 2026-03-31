b72eff065a44e52c26e5732b0858523e9b241756
johndoe <johndoe@email.com>
2026-03-31 10:31:31 +0300

    бновление 3

--- news_aggregator/settings.py
+++ news_aggregator/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vw5i+tve@x)$s05(rfk=&wf5y1zua%+-xx$c)kfyixm=qb4#c-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yulik.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
